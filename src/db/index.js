a4258b01943999f3ad5fe517c16743e1754b1358
Pritam <pritamsamaddar840@gmail.com>
2025-07-25 10:40:20 +0530

    fix kafka

--- src/db/index.js
+++ src/db/index.js
@@ -1,30 +1,30 @@
-const { Pool } = require('pg');
-const config = require('../config');
-const logger = require('../utils/logger');
+const { Pool } = require("pg");
+const config = require("../config");
+const logger = require("../utils/logger");
 
 const db = new Pool({
-    user: config.db.user,
-    host: config.db.host,
-    database: config.db.name,
-    password: config.db.password,
-    port: config.db.port,
-    max: 10,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 5000,
-    // ssl: {
-    //     rejectUnauthorized: false,
-    //   },
+  user: config.db.user,
+  host: config.db.host,
+  database: config.db.name,
+  password: config.db.password,
+  port: config.db.port,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-db.on('connect', () => {
-    logger.info(`Connected to PostgreSQL database: ${config.db.name}`);
+db.on("connect", () => {
+  logger.info(`Connected to PostgreSQL database: ${config.db.name}`);
 });
 
-db.on('error', (err) => {
-    logger.error(`PostgreSQL database error: ${err.message}`, err);
+db.on("error", (err) => {
+  logger.error(`PostgreSQL database error: ${err.message}`, err);
 });
 
-module.exports = db; 
+module.exports = db;
 
 // src/db.js
 // const { Pool } = require("pg");
